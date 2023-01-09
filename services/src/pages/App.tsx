c1026a4d9d6abe8e7fc70c8464e758c6ec8bf28c
Anh Nhat <phananhnhat2@gmail.com>
2023-01-09 16:30:35 +0700

    [FE] init project

---
+++ services/src/pages/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="text-center">
+          <h1>Wellcome to FOUR</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
