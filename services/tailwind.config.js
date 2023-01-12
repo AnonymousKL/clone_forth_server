878ef00140328ebb883114a10f9ed1f7b1f6665d
Anh Nhat <phananhnhat2@gmail.com>
2023-01-12 18:45:30 +0700

    [FE] feat: add clsx pkg, config tailwind

--- services/tailwind.config.js
+++ services/tailwind.config.js
@@ -1,8 +1,29 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          1: '#2A5CC5',
+        },
+      },
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+        // heading: ['Roboto', ...defaultTheme.fontFamily.sans],
+      },
+      borderRadius: {
+        5: '0.3125rem',
+        10: '0.625rem',
+        20: '1.25rem',
+        25: '1.5625rem',
+        30: '1.875rem',
+      },
+      boxShadow: {
+        '4p': '0 4px 4px rgba(0, 0, 0, 0.25)',
+      }
+    },
   },
   plugins: [],
   darkMode: 'class',
