b283d2d89056656e4f27a52562d28124065a1add
Anh Nhat <phananhnhat2@gmail.com>
2023-01-12 11:01:11 +0700

    [FE] feat: add i18n

--- services/src/index.tsx
+++ services/src/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import router from './routes';
-import './styles/index.scss';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import Theme from 'components/Theme';
+import router from 'routes';
+import 'styles/index.scss';
+
+import en from 'i18n/en.json'
+import vi from 'i18n/vi.json'
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: en },
+    vi: { translation: vi },
+  },
+  lng: "vi",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +28,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Theme>
+      <RouterProvider router={router} />
+    </Theme>
   </React.StrictMode>
 );
