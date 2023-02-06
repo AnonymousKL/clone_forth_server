6962bbedcc342e185326b06d79783292b9d53dad
Anh Nhat <phananhnhat2@gmail.com>
2023-02-06 17:24:23 +0700

    [FE] chore: disable mock, add i18n text

--- services/src/__mocks/index.ts
+++ services/src/__mocks/index.ts
@@ -3,14 +3,14 @@ import AxiosMockAdapter from "axios-mock-adapter";
 
 const axiosInstance = axios.create()
 
-const mockAdapterInstance = new AxiosMockAdapter(axiosInstance, {
-  delayResponse: 1000,
-  // onNoMatch: 'passthrough',
-})
+// const mockAdapterInstance = new AxiosMockAdapter(axiosInstance, {
+//   delayResponse: 1000,
+//   // onNoMatch: 'passthrough',
+// })
 
-const config = require('./mock-data.json')
-config.reduce((adapter: AxiosMockAdapter, { path, data }: { path: string, data: any }) => {
-  return path ? adapter.onAny(path).reply(200, data) : adapter
-}, mockAdapterInstance)
+// const config = require('./mock-data.json')
+// config.reduce((adapter: AxiosMockAdapter, { path, data }: { path: string, data: any }) => {
+//   return path ? adapter.onAny(path).reply(200, data) : adapter
+// }, mockAdapterInstance)
 
 export default axiosInstance
